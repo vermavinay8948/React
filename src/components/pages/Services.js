03101cd0562c558f88464ad2d69f9d633f44acdb
Vinay Verma <56758330+vermavinay8948@users.noreply.github.com>
2022-06-30 16:25:57 +0530

    Add files via upload

---
+++ src/components/pages/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+
+import Footer from '../Footer';
+
+export default function Services() {
+  return (
+    <>
+  <h1 className='services'>SERVICES</h1>
+  <Footer />
+ </>
+  )
+}
